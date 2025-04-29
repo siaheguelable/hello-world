510c1de9524b2ef7a0f97d8c8a7b8cfe1ebe2c00
siaheguelable <siaheguelable@gmail.com>
2025-04-30 00:48:45 +0200

    first react app

---
+++ src/components/Greet.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// normal function
+// function Greet() {   
+ /*function Greet() {
+  return  <h1>Hello Curtis</h1>
+   } */
+
+// The above code defines a functional component named Greet that returns a simple JSX element displaying "Hello Curtis".
+
+/*
+const Greet= () => {
+    return <h1>Hello Curtis</h1>
+    
+}*/
+
+
+// more concise arrow function
+ //export
+const Greet=()=> <h1>HelloCurtis</h1>;
+export default Greet;
